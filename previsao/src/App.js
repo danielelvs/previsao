e0ea3071f0aefc36debefe9b74f3cdb21e0e5e01
Daniele Santos <daniele.santos@trustly.com>
2021-10-01 23:04:05 -0300

    Implementação de lógica

--- previsao/src/App.js
+++ previsao/src/App.js
@@ -1,34 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import X2JS from "x2js";
+import axios from "axios";
 
 function App() {
- 
-const [temperatura, setTemperatura] = useState({})
+  const [temperatura, setTemperatura] = useState();
 
-  useEffect(() =>{
-  temp()
-},[])
+  useEffect(() => {
+    obtemObj();
+  }, []);
 
-const temp = () => {
-   axios
-   .get("https://api.factmaven.com/xml-to-json/?xml=http://servicos.cptec.inpe.br/XML/capitais/condicoesAtuais.xml")
-   
-   .then(res => {
-       console.log(res.data.capitais.metar)
-       setTemperatura(res.data.capitais.metar)
-   })
-   .catch(err => {
+  async function obtemObj() {
+    await axios
+      .get("http://servicos.cptec.inpe.br/XML/capitais/condicoesAtuais.xml")
+      .then((res) => {
+        const x2js = new X2JS();
+        const data = x2js.xml2js(res.data);
+        const localizacoes = data.capitais.metar;
+        let resultado = [];
+
+        // localizacoes.map((linha) => console.log(linha));
+
+        localizacoes.forEach((linha) => {
+          if (
+            linha.codigo === "SBJP" ||
+            linha.codigo === "SBAR" ||
+            linha.codigo === "SBBE" ||
+            linha.codigo === "SBCF"
+          ) {
+            resultado.push(linha);
+          }
+        });
+        console.log(resultado);
+        setTemperatura(resultado);
+      })
+      .catch((err) => {
         console.log(err);
-        
-   })
-}
+      });
+  }
 
-return(
-  <div>
-    <h1>PREVISÃO DO TEMPO - REGIÃO SUDESTE</h1>
-   
-  </div>
-)
-
+  return (
+    <div>
+      <h1>PREVISÃO DO TEMPO - REGIÃO SUDESTE</h1>
+      {temperatura[0].codigo}
+    </div>
+  );
 }
-export default App
+export default App;
